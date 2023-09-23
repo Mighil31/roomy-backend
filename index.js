c33c6133386b1f045ad6550a4702dd8c5f4aad35
mighil31 <mighildath@gmail.com>
2023-09-23 12:36:23 +0530

    CORS

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const app = express();
 
 app.use(express.json({ extended: false }));
 
+// CORS
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.use("/api/post", Post);
 app.use("/api/user", User);
 app.use("/api/auth", Auth);
