125d84153d58a9bb88b1f11a40acaeaa51f75d8a
mighil31 <mighildath@gmail.com>
2024-02-04 15:58:10 +0530

    try socket url

--- utils/sockets.js
+++ utils/sockets.js
@@ -2,6 +2,7 @@ import { Server } from "socket.io";
 
 const initializeSocket = (server) => {
   const io = new Server(server, {
+    path: "http://35.154.214.36/socket",
     cors: {
       origin: "*",
       methods: ["GET", "POST"],
@@ -24,6 +25,13 @@ const initializeSocket = (server) => {
     socket.on("disconnect", () => {
       console.log("User Disconnected", socket.id);
     });
+  });
+
+  io.engine.on("connection_error", (err) => {
+    console.log(err.req); // the request object
+    console.log(err.code); // the error code, for example 1
+    console.log(err.message); // the error message, for example "Session ID unknown"
+    console.log(err.context); // some additional error context
   });
 
   return io;
