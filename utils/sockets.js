9ebf3da5e8ab10b9515d47d4560623518aff13c6
mighil31 <mighildath@gmail.com>
2024-02-04 14:02:29 +0530

    localhost changes for deploy

--- utils/sockets.js
+++ utils/sockets.js
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 const initializeSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: "35.154.214.36",
       methods: ["GET", "POST"],
     },
   });
